868751f873b075b0bbd7936314f7308c27f8d236
Stephanie <kann.zaubern@gmail.com>
2014-07-14 23:08:00 +0200

    made new layout for textitem_in_listview

--- 24HoursOfFame/app/src/main/java/com/hoursoffame/android/ui/adapters/ListViewAdapter.java
+++ 24HoursOfFame/app/src/main/java/com/hoursoffame/android/ui/adapters/ListViewAdapter.java
@@ -1,14 +1,19 @@
 package com.hoursoffame.android.ui.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.hoursoffame.android.R;
 import com.hoursoffame.android.data.TextItem;
 
 public class ListViewAdapter extends ArrayAdapter<TextItem> {
+
+    TextItem textItem;
 
     public ListViewAdapter(Context context, TextItem[] textItems) {
         super(context, 0, textItems);
@@ -16,11 +21,26 @@ public class ListViewAdapter extends ArrayAdapter<TextItem> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView textView = new TextView(getContext());
+
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        RelativeLayout relLayout = (RelativeLayout) inflater.inflate(R.layout.textitem_in_listview, null);
+
+        textItem = getItem(position);
+
+        TextView titleTv = (TextView) relLayout.findViewById(R.id.title);
+        TextView textTv = (TextView) relLayout.findViewById(R.id.text);
+        TextView timestampTv = (TextView) relLayout.findViewById(R.id.timestamp);
+
+        titleTv.setText(textItem.getTitle());
+        textTv.setText(textItem.getText());
+        timestampTv.setText(textItem.getCreationTime());
+
+
+        /*TextView textView = new TextView(getContext());
         TextItem textItem = getItem(position);
 
         textView.setText(textItem.getTitle() + " / " + textItem.getText() + " / " + textItem.getCreationTime());
-        textView.setPadding(10, 10, 10, 10);
-        return textView;
+        textView.setPadding(10, 10, 10, 10); */
+        return relLayout;
     }
 }
